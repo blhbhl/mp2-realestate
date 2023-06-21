de1dcb22bebf50be1e9c49920f6df632ccfe3b51
Irvin-05 <irvinchristian.operio@gmail.com>
2023-06-22 04:01:25 +0800

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
         <div className="flexColStart f-left">
           <img src="./logo2.png" alt="" width={120} />
           <span className="secondaryText">
-            Our vision is to make all people <br />
-            the best place to live for them.
+            Our vision is to give all people <br />
+            the best place for them to live.
           </span>
         </div>
 
