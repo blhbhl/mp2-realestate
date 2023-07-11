b9dec6e0ae67555335a3e2c0d215967a78885243
hmmllnvhnn <manlanothammillan@gmail.com>
2023-07-11 20:14:58 +0800

    add pure css

--- frontend/src/components/BuyerPage/AppPage.jsx
+++ frontend/src/components/BuyerPage/AppPage.jsx
@@ -1,86 +1,83 @@
 import React from 'react'
 import BuyerPage from './BuyerPage'
 import contents from './contents'
-import Header from '../Header/Header'
 import { BiSolidRightArrow, BiSolidLeftArrow } from 'react-icons/bi'
+import './AppPage.css'
 
 const AppPage = () => {
     return (
-        <div>
-            <Header />
-            <div className='overflow-x-hidden'>
+        <div className='page'>
+            <div>
                 <div>
-                    <div>
-                    </div>
-                    <div className='flex justify-center text-center'>
-                        <div class="relative mb-3" data-te-input-wrapper-init>
-                            <input
-                                type="search"
-                                class="peer border-slate-950 block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black dark:placeholder:text-black dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                id="search"
-                                placeholder="Type query" />
-                            <label
-                                for="labelSearch"
-                                class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-black transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-black dark:peer-focus:text-primary"
-                            >
-                                Location
-                            </label>
-                        </div>
-                    </div>
-                    <div className='App'>
-                        {contents.map(contents => (
-                            <BuyerPage
-                                key={contents.id}
-                                image={contents.image}
-                                name={contents.name}
-                                description={contents.description}
-                                price={contents.price}
-                                address={contents.address}
-                                location={contents.location}
-                                bed={contents.bed}
-                                bath={contents.bath}
-                                sqms={contents.sqms}
-                            />
-                        ))}
+                </div>
+                <div className='search'>
+                    <div class="position" data-te-input-wrapper-init>
+                        <input
+                            type="search"
+                            class="peer border-slate-950 block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-black dark:placeholder:text-black dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                            id="search"
+                            placeholder="Type query" />
+                        <label
+                            for="labelSearch"
+                            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-black transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-black dark:peer-focus:text-primary"
+                        >
+                            Location
+                        </label>
                     </div>
                 </div>
-                <div>
-                    <div className='flex text-center justify-center ml-[8px] mt-[140px] mb-20'>
-                        <nav aria-label="Page navigation example">
-                            <ul class="flex items-center -space-x-px h-10 text-base text-gray-600">
-                                <li>
-                                    <a href="#" class="flex items-center justify-center bg-none">
-                                        <span class="sr-only">Previous</span>
-                                        <BiSolidLeftArrow className='text-[#1F3E72] text-xl' />
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="flex items-center justify-center rounded-3xl px-4 mx-3 h-10 shadow-sm shadow-gray-400 hover:bg-gray-400 hover:text-white bg-white">1</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="flex items-center justify-center rounded-3xl px-4 mx-3 h-10 shadow-sm shadow-gray-400 leading-tight hover:bg-gray-400 hover:text-white bg-white">2</a>
-                                </li>
-                                <li>
-                                    <a href="#" aria-current="page" class="z-10 flex items-center justify-center rounded-3xl px-4 h-10 mx-3 shadow-sm shadow-gray-400 leading-tight hover:bg-[#1F3E72] hover:text-white bg-[#FFA500]">3</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="flex items-center justify-center rounded-3xl px-4 mx-3 h-10 shadow-sm shadow-gray-400 leading-tight hover:bg-gray-400 hover:text-white bg-white">4</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="flex items-center justify-center rounded-3xl px-4 mx-3 h-10 shadow-sm shadow-gray-400 leading-tight hover:bg-gray-400 hover:text-white bg-white">5</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="flex items-center justify-center bg-none">
-                                        <span class="sr-only">Next</span>
-                                        <BiSolidRightArrow className='text-[#1F3E72] text-xl' />
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+                <div className='App'>
+                    {contents.map(contents => (
+                        <BuyerPage
+                            key={contents.id}
+                            image={contents.image}
+                            name={contents.name}
+                            description={contents.description}
+                            price={contents.price}
+                            address={contents.address}
+                            location={contents.location}
+                            bed={contents.bed}
+                            bath={contents.bath}
+                            sqms={contents.sqms}
+                        />
+                    ))}
                 </div>
-            </div >
-        </div>
+            </div>
+            <div>
+                <div className='pagination'>
+                    <nav aria-label="Page navigation example">
+                        <ul class="ulclass">
+                            <li>
+                                <a href="#" class="previous">
+                                    <span class="sr-only">Previous</span>
+                                    <BiSolidLeftArrow className='left text-[#1F3E72] text-xl' />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="nonactive">1</a>
+                            </li>
+                            <li>
+                                <a href="#" class="nonactive">2</a>
+                            </li>
+                            <li>
+                                <a href="#" aria-current="page" class="active">3</a>
+                            </li>
+                            <li>
+                                <a href="#" class="nonactive">4</a>
+                            </li>
+                            <li>
+                                <a href="#" class="nonactive">5</a>
+                            </li>
+                            <li>
+                                <a href="#" class="next">
+                                    <span class="sr-only">Next</span>
+                                    <BiSolidRightArrow className='right' />
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div >
     )
 }
 
--- frontend/src/components/BuyerPage/BuyerPage.jsx
+++ frontend/src/components/BuyerPage/BuyerPage.jsx
@@ -1,97 +1,102 @@
 import React, { useState } from 'react'
 import { LuBath, LuBedDouble } from 'react-icons/lu'
 import { BsBookmarkHeart, BsBookmarkHeartFill } from 'react-icons/bs'
+import './BuyerPage.css'
 
 const BuyerPage = (props) => {
     const [isActive, setIsActive] = useState(false);
     const [modalOpen, setModalOpen] = useState(false);
 
-    function Modal({ setOpenModal }) {
+    const Modal = ({ setOpenModal }) => {
+        if (!open) return null
         return (
-            <div className="modalBackground">
-                <div className="modalContainer">
-                    <div>
-                        <section class="text-gray-600 body-font">
-                            <h3 className='text-[#1F3E72] text-4xl decoration-2 mr-[545px] mt-[65px]'>{props.name}</h3>
-                            <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                                <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                                    </h1>
-                                    <div className='ml-6'>
-                                        <div class=" text-gray-600">
-                                            <div className='mt-[-110px]'>
-                                                <h2 className='text-lg'>Description</h2>
-                                                <hr className='bg-gray-800' />
-                                                <p>{props.description}</p>
+            <>
+                <div className="overlay">
+                    <div className="modalContainer">
+                        <div>
+                            <section class="modal">
+                                <div className='text'>
+                                    <h3 className='name'>{props.name}</h3>
+                                </div>
+                                <div class="container">
+                                    <div class="white">
+                                        <h1 class="title">
+                                        </h1>
+                                        <div className='describe'>
+                                            <div class="color">
+                                                <div className='describe1'>
+                                                    <h2 className='title-name'>Description</h2>
+                                                    <hr className='line' />
+                                                    <p>{props.description}</p>
+                                                </div>
+                                                <div>
+                                                    <h2 className='title-location'>Location</h2>
+                                                    <hr className='line1' />
+                                                    <p className='location'>{props.location} </p>
+                                                </div>
                                             </div>
-                                            <div>
-                                                <h2 className='text-lg'>Location</h2>
-                                                <hr className='bg-gray-800' />
-                                                <p className='flex mt-3 mb-[-10px]'>{props.location} </p>
+                                        </div>
+                                        <div className='bottom'>
+                                            <br />
+                                            <hr className='hrline' />
+                                            <div className='bottom1'>
+                                                <h1 className='same4'>Beds</h1>
+                                                <p className='bottom2'>{props.bed}</p>
                                             </div>
+                                            <hr className='hrline' />
+                                            <div className='bottom1'>
+                                                <h1 className='same5'>Bathrooms</h1>
+                                                <p className='bottom2'>{props.bath}</p>
+                                            </div>
+                                            <hr className='hrline' />
+                                            <div className='bottom1'>
+                                                <h1 className='same6'>Sqms</h1>
+                                                <p className='bottom2'>{props.sqms}</p>                                            </div>
+                                            <hr className='hrline2' />
+                                        </div>
+                                        <div className="top">
+                                            <h2><span className='pesos1'>₱</span>{props.price}</h2>
+                                            <button className="btn">Talk to Agent!</button>
                                         </div>
                                     </div>
-                                    <div className='text-gray-600 ml-6'>
-                                        <br />
-                                        <hr className='w-[350px] bg-gray-800' />
-                                        <div className='flex'>
-                                            <h1 className='mt-1 mb-1'>Beds</h1>
-                                            <p className='mt-[5px] ml-[260px]'>{props.bed}</p>
-                                        </div>
-                                        <hr className='w-[350px] bg-gray-800' />
-                                        <div className='flex'>
-                                            <h1 className='flex mt-1 mb-1'>Bathrooms</h1>
-                                            <p className='flex mt-[5px] ml-[180px]'>{props.bath}</p>
-                                        </div>
-                                        <hr className='w-[350px] bg-gray-800' />
-                                        <div className='flex'>
-                                            <h1 className='mt-1 mb-1'>Sqms</h1>
-                                            <p className='mt-[5px] ml-[270px]'>{props.sqms}</p>
-                                        </div>
-                                        <hr className='w-[350px] bg-gray-800' />
-                                    </div>
-                                    <div className="flex justify-center ml-6 text-3xl mt-2">
-                                        <h2><span className='text-[#FFA500]'>₱</span>{props.price}</h2>
-                                        <button className="inline-flex text-white justify-center text-center ml-[420px] mt-[-50px] mb-[40px] w-[200px] bg-[#FFA500] border-0 py-2 px-6 focus:outline-none hover:bg-[#1F3E72] rounded text-lg">Talk to Agent!</button>
+                                    <div className="main">
+                                        <img className="img" alt="hero" src={props.image} />
                                     </div>
                                 </div>
-                                <div className="lg:max-w-lg lg:w-full md:w-1/2 mt-[-200px] w-[300px]">
-                                    <img className="object-cover object-center rounded w-[970px] h-[300px] mt-[5px] ml-[-20px]" alt="hero" src={props.image} />
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                    <div className="footer">
-                        <button
-                            onClick={() => {
-                                setOpenModal(false);
-                            }}
-                            id="cancelBtn"
-                            className='bg-red-700 text-white p-2 ml-[-70px] mt-2 rounded-md'
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>
-            </div>
+                            </section>
+                        </div>
+                        <div className="footer">
+                            <button
+                                onClick={() => {
+                                    setOpenModal(false);
+                                }}
+                                id="cancelBtn"
+                                className='cancel'
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div >
+                </div >
+            </>
         )
     }
 
     return (
         <div className=''>
 
-            <div className='box-content scroll-smooth w-full'>
+            <div className='box'>
                 <div className='box-content'>
-                    <div className='container'>
-                        <section className="productList bg-white h-screen text-gray-400 body-font">
-                            <div className="container px-3 py-24 h-[100px]">
+                    <div className=''>
+                        <section className="productList">
+                            <div className="container1">
                                 <div className="">
-                                    <img className='h-[200px] w-[700px] rounded-t-2xl' src={props.image} />
-                                    <div key={props.id} className="p-3 shadow-xl rounded-xl w-[350px]">
-                                        <div className="bg-white bg-opacity-40 p-4 rounded-lg">
-                                            <div className='flex mb-[-270px] p-[-10px]'>
-                                                <h3 className="tracking-widest text-gray-600  text-2xl title-font font-semibold mt-[10px]"><span className='text-[#FFA500]'>₱</span>{props.price}</h3>
-                                                <div className="flex cursor-pointer select-none text-[#FFA500] text-[45px] ml-[115px] mr-[-20px] mt-[-15px]">
+                                    <img className='img1' src={props.image} />
+                                    <div key={props.id} className="id">
+                                        <div className="card">
+                                            <div className='icons'>
+                                                <h3 className="icons1"><span className='pesos'>₱</span>{props.price}</h3>
+                                                <div className="icons2">
                                                     {isActive ? <BsBookmarkHeartFill onClick={() => {
                                                         setIsActive(!isActive)
                                                     }} /> :
@@ -101,25 +106,27 @@ const BuyerPage = (props) => {
                                                     }
                                                 </div>
                                             </div>
-                                            <h3 className="text-3xl text-[#1F3E72] title-font mb-[-30px] mt-[205px] font-bold">{props.name}</h3>
-                                            <p className="leading-relaxed text-gray-600 mb-[20px] mt-[60px]">{props.address}</p>
-                                            <hr className='mb-4' />
-                                            <div className='flex justify-center text-[#FFA500]'>
-                                                <div className='flex pl-1 pr-1'><h2 className='flex text-[15px] text-gray-600 ml-3 mr-3 mt-1'>{props.bed}</h2><LuBedDouble className='text-3xl' /></div>
-                                                <div className='flex pl-1 pr-1'><h2 className='flex text-[15px] text-gray-600 ml-3 mr-3 mt-1'>{props.bath}</h2><LuBath className='text-3xl' /></div>
+                                            <h3 className="name1">{props.name}</h3>
+                                            <p className="address">{props.address}</p>
+                                            <hr className='hrline2' />
+                                            <div className='div'>
+                                                <div className='bed1'><h2 className='bed2'>{props.bed}</h2><LuBedDouble className='txt' /></div>
+                                                <div className='bed1'><h2 className='bed2'>{props.bath}</h2><LuBath className='txt' /></div>
                                             </div>
-                                            <div className='text-center ml-[590px] mt-[25px]'>
-                                                <div className="App">
-                                                    <button
-                                                        className="openModalBtn bg-[#FFA500] w-[200px] p-3 rounded-lg text-white"
-                                                        onClick={() => {
-                                                            setModalOpen(true);
-                                                        }}
-                                                    >
-                                                        See More!
-                                                    </button>
+                                            <div>
+                                                <div className='buttonModal'>
+                                                    <div className="App">
+                                                        <button
+                                                            className="openModalBtn"
+                                                            onClick={() => {
+                                                                setModalOpen(true);
+                                                            }}
+                                                        >
+                                                            See More!
+                                                        </button>
 
-                                                    {modalOpen && <Modal setOpenModal={setModalOpen} />}
+                                                        {modalOpen && <Modal setOpenModal={setModalOpen} />}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
