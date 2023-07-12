f987614d1fd69104e7be4a90090b999db69292e2
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-07-12 19:45:32 +0800

    update valuation form

--- frontend/src/components/Seller/Seller.jsx
+++ frontend/src/components/Seller/Seller.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Seller.css";
 
 const Seller = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [propertyType, setPropertyType] = useState('');
-  const [propertyArea, setPropertyArea] = useState('');
-  const [propertyAge, setPropertyAge] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [propertyType, setPropertyType] = useState("");
+  const [propertyArea, setPropertyArea] = useState("");
+  const [propertyAge, setPropertyAge] = useState("");
+  const [additionalProperties, setAdditionalProperties] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Perform actions with the form values
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Property Type:', propertyType);
-    console.log('Property Area:', propertyArea);
-    console.log('Property Age:', propertyAge);
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Phone Number:", phoneNumber);
+    console.log("Address:", address);
+    console.log("Property Type:", propertyType);
+    console.log("Property Area:", propertyArea);
+    console.log("Property Age:", propertyAge);
+    console.log("Additional Properties:", additionalProperties);
   };
 
   return (
@@ -42,6 +48,24 @@ const Seller = () => {
                   type="email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+
+              <label>
+                Phone Number:
+                <input
+                  type="tel"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+              </label>
+
+              <label>
+                Address:
+                <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
                 />
               </label>
 
@@ -73,6 +97,15 @@ const Seller = () => {
                 onChange={(e) => setPropertyAge(e.target.value)}
               />
 
+              <label className="textarea-label">
+                Additional Properties:
+                <textarea
+                  className="textarea-input"
+                  value={additionalProperties}
+                  onChange={(e) => setAdditionalProperties(e.target.value)}
+                ></textarea>
+              </label>
+
               <button type="submit">Submit</button>
             </form>
           </div>
@@ -83,5 +116,3 @@ const Seller = () => {
 };
 
 export default Seller;
-
-
