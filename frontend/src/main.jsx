9ac2e670e58a959fbf1821b20914f02e9e66f306
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-07-12 11:27:09 +0800

    fix link again

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,8 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-// import AppPage from "./components/BuyerPage/AppPage";
-// import SellAHome from "./pages/SellAHome/SellAHome";
+import AppPage from "./components/BuyerPage/AppPage";
+import SellAHome from "./pages/SellAHome/SellAHome";
 
 
 const router = createBrowserRouter([
@@ -26,8 +26,17 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterForm/>,
-  }
+  }, 
 
+  {
+    path: "/buy-a-home",
+    element: <AppPage />,
+  },
+
+  {
+    path: "/sell-a-home",
+    element: <SellAHome />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
