110161afea53e1d082dde5a8a940f0de3c5bf267
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-07-12 18:35:48 +0800

    add form info

--- frontend/src/components/Seller/Seller.jsx
+++ frontend/src/components/Seller/Seller.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import "./Seller.css";
 
 const Seller = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [propertyType, setPropertyType] = useState('');
   const [propertyArea, setPropertyArea] = useState('');
   const [propertyAge, setPropertyAge] = useState('');
@@ -9,42 +11,77 @@ const Seller = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Perform actions with the form values
+    console.log('Name:', name);
+    console.log('Email:', email);
     console.log('Property Type:', propertyType);
     console.log('Property Area:', propertyArea);
     console.log('Property Age:', propertyAge);
   };
 
   return (
-
     <section className="for-sale" id="sale">
-  <div id="sale" className="s-wrapper">
-  <div className="paddings innerWidth r-container">
-    <p className="orangeText">Sell A Home</p>
-    <h2 className="primaryText">Valuation Request Form</h2>
-    <div className="form">
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="propertyType">Property Type:</label>
-      <select id="propertyType" value={propertyType} onChange={(e) => setPropertyType(e.target.value)}>
-        <option value="">Select property type</option>
-        <option value="house">House</option>
-        <option value="apartment">Apartment</option>
-        <option value="condo">Condo</option>
-      </select>
+      <div id="sale" className="s-wrapper">
+        <div className="paddings innerWidth r-container">
+          <p className="orangeText">Sell A Home</p>
+          <h2 className="primaryText">Valuation Request Form</h2>
 
-      <label htmlFor="propertyArea">Property Area (in sqft):</label>
-      <input type="number" id="propertyArea" value={propertyArea} onChange={(e) => setPropertyArea(e.target.value)} />
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </label>
 
-      <label htmlFor="propertyAge">Property Age:</label>
-      <input type="number" id="propertyAge" value={propertyAge} onChange={(e) => setPropertyAge(e.target.value)} />
+              <label>
+                Email:
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
 
-      <button type="submit">Submit</button>
-    </form>
-    </div>
+              <label htmlFor="propertyType">Property Type:</label>
+              <select
+                id="propertyType"
+                value={propertyType}
+                onChange={(e) => setPropertyType(e.target.value)}
+              >
+                <option value="">Select property type</option>
+                <option value="house">House</option>
+                <option value="apartment">Apartment</option>
+                <option value="condo">Condo</option>
+              </select>
 
-    </div>
-  </div>
-</section>
+              <label htmlFor="propertyArea">Property Area (in sqft):</label>
+              <input
+                type="number"
+                id="propertyArea"
+                value={propertyArea}
+                onChange={(e) => setPropertyArea(e.target.value)}
+              />
+
+              <label htmlFor="propertyAge">Property Age:</label>
+              <input
+                type="number"
+                id="propertyAge"
+                value={propertyAge}
+                onChange={(e) => setPropertyAge(e.target.value)}
+              />
+
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
 export default Seller;
+
+
