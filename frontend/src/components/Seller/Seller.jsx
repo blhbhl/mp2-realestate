f6b1a688a4ed6cc750f464a40bbdf0c95cd87595
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-07-18 17:46:51 +0800

    add image input to valuation form

--- frontend/src/components/Seller/Seller.jsx
+++ frontend/src/components/Seller/Seller.jsx
@@ -13,88 +13,110 @@ const Seller = () => {
     propertyType: "",
     propertyArea: "",
     propertyAge: "",
-    additionalProperties: ""
+    additionalProperties: "",
+    image: null // Add image property to the state
   });
 
   const navigate = useNavigate();
   const [errors, setErrors] = useState({});
   const handleInput = (e) => {
-    setValues(prev => ({...prev, [e.target.name]: [e.target.value]}))
+    if (e.target.name === "image") {
+      // If the input field is an image, access the file using e.target.files[0]
+      setValues(prev => ({...prev, [e.target.name]: e.target.files[0]}));
+    } else {
+      setValues(prev => ({...prev, [e.target.name]: e.target.value}));
+    }
   };
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+
     const err = Validation(values);
     setErrors(err);
-    if(err.name === "" && 
-      err.email === "" && 
+    if (
+      err.name === "" &&
+      err.email === "" &&
       err.phoneNumber === "" &&
       err.address === "" &&
       err.propertyType === "" &&
       err.propertyArea === "" &&
-      err.propertyAge === "" ) {
-      axios.post('http://localhost:3001/sell-a-home', values)
-      .then(response => {
-      if(response.data === "Failed") {
-        navigate('/');
-        alert("Submitted successfully!");
-      } else {
-          alert("Unable to submit the form!");
+      err.propertyAge === ""
+    ) {
+      const formData = new FormData();
+      formData.append("image", values.image); // Append the image to the form data
+
+      // Append other form values to the form data
+      for (const key in values) {
+        if (key !== "image") {
+          formData.append(key, values[key]);
+        }
       }
-    })
-    .catch(error => {
-      console.error('Error inserting seller: ', error);
-    });
-  }
-};
+
+      axios
+        .post("http://localhost:3001/sell-a-home", formData)
+        .then((response) => {
+          if (response.data === "Failed") {
+            navigate("/");
+            alert("Submitted successfully!");
+          } else {
+            alert("Unable to submit the form!");
+          }
+        })
+        .catch((error) => {
+          console.error("Error inserting seller: ", error);
+        });
+    }
+  };
+
   return (
     <section className="for-sale" id="sale">
       <div id="sale" className="s-wrapper">
-        <div className="paddings innerWidth r-container">
+        <div className="paddings innercd Width r-container">
           <p className="orangeText p-marg">Sell A Home</p>
           <h2 className="primaryText">Valuation Request Form</h2>
 
           <div className="form">
             <form onSubmit={handleSubmit}>
               <label>Name:</label>
-                <input
-                  type="text"
-                  name="name"
-                  onChange={handleInput}
-                  placeholder="Enter FullName"
-                />
-                 {errors.name && <span className="span-red">{errors.name}</span>}
-              
+              <input
+                type="text"
+                name="name"
+                onChange={handleInput}
+                placeholder="Enter FullName"
+              />
+              {errors.name && <span className="span-red">{errors.name}</span>}
 
               <label>Email:</label>
-                <input
-                  type="email"
-                  name="email"
-                  onChange={handleInput}
-                  placeholder="Enter Email"
-                />
-                  {errors.email && <span className="span-red">{errors.email}</span>}
-              
+              <input
+                type="email"
+                name="email"
+                onChange={handleInput}
+                placeholder="Enter Email"
+              />
+              {errors.email && <span className="span-red">{errors.email}</span>}
 
               <label>Phone Number:</label>
-                <input
-                  type="tel"
-                  name="phoneNumber"
-                  onChange={handleInput}
-                  placeholder="Enter Phone Number"
-                />
-                  {errors.phoneNumber && <span className="span-red">{errors.phoneNumber}</span>}
-              
+              <input
+                type="tel"
+                name="phoneNumber"
+                onChange={handleInput}
+                placeholder="Enter Phone Number"
+              />
+              {errors.phoneNumber && (
+                <span className="span-red">{errors.phoneNumber}</span>
+              )}
+
               <label>Address:</label>
-                <input
-                  type="text"
-                  name="address"
-                  onChange={handleInput}
-                  placeholder="Enter Address"
-                />
-                 {errors.address && <span className="span-red">{errors.address}</span>}
-              
+              <input
+                type="text"
+                name="address"
+                onChange={handleInput}
+                placeholder="Enter Address"
+              />
+              {errors.address && (
+                <span className="span-red">{errors.address}</span>
+              )}
+
               <label htmlFor="propertyType">Property Type:</label>
               <select
                 id="propertyType"
@@ -106,7 +128,9 @@ const Seller = () => {
                 <option value="apartment">Apartment</option>
                 <option value="condo">Condo</option>
               </select>
-              {errors.propertyType && <span className="span-red">{errors.propertyType}</span>}
+              {errors.propertyType && (
+                <span className="span-red">{errors.propertyType}</span>
+              )}
 
               <label htmlFor="propertyArea">Property Area (in sqft):</label>
               <input
@@ -116,8 +140,10 @@ const Seller = () => {
                 onChange={handleInput}
                 placeholder="Enter Property Area"
               />
-               {errors.propertyArea && <span className="span-red">{errors.propertyArea}</span>}
-              
+              {errors.propertyArea && (
+                <span className="span-red">{errors.propertyArea}</span>
+              )}
+
               <label htmlFor="propertyAge">Property Age:</label>
               <input
                 type="number"
@@ -130,15 +156,24 @@ const Seller = () => {
               <label className="textarea-label">
                 Additional Properties:
               </label>
-                <textarea
-                  className="textarea-input"
-                  name="additionalProperties"
-                  onChange={handleInput}
-                  placeholder="Enter Additional Properties"
-                ></textarea>
-              
+              <textarea
+                className="textarea-input"
+                name="additionalProperties"
+                onChange={handleInput}
+                placeholder="Enter Additional Properties"
+              ></textarea>
 
-              <button type="submit" className="button">Submit</button>
+              <label>Image:</label>
+              <input
+                type="file"
+                name="image"
+                onChange={handleInput}
+                accept="image/*"
+              />
+
+              <button type="submit" className="button">
+                Submit
+              </button>
             </form>
           </div>
         </div>
