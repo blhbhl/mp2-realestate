b3c222c546a513322455d6279b6f7cd0eeec9cbd
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-07-19 22:48:20 +0800

    fix stuff

--- frontend/src/components/Residencies/Residencies.jsx
+++ frontend/src/components/Residencies/Residencies.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../../utils/slider.json";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 // Import Swiper styles
@@ -7,15 +7,22 @@ import "./Residencies.css";
 import { sliderSettings } from "../../utils/common";
 
 const Residencies = () => {
+  const [progress, setProgress] = useState('begin');
+  const [activeIndex, setActiveIndex] = useState();
+
+  const onChange = (index) => {
+    setActiveIndex(index.activeIndex);
+  }
+
   return (
     <div id="residencies" className="r-wrapper">
       <div className="paddings innerWidth r-container">
         <div className="flexColStart r-head">
-          <span className="orangeText">Best Choices</span>
+          <span className="orangeText">Best Choices </span>
           <span className="primaryText">Popular Residencies</span>
         </div>
-        <Swiper {...sliderSettings}>
-          <SlideNextButton />
+        <Swiper {...sliderSettings}  onActiveIndexChange={onChange} >
+          <SlideNextButton activeIndex={activeIndex} />
           {/* slider */}
           {data.map((card, i) => (
             <SwiperSlide key={i}>
@@ -41,14 +48,16 @@ const Residencies = () => {
 
 export default Residencies;
 
-const SlideNextButton = () => {
+const SlideNextButton = ({ activeIndex }) => {
   const swiper = useSwiper();
   return (
-    <div className="flexCenter r-buttons">
-      <button onClick={() => swiper.slidePrev()} className="r-prevButton">
+    <div className="flex absolute right-0 -top-24">
+      <button onClick={() => swiper.slidePrev()}
+       className="transition-all mr-2 text-blue-600 py-2 px-4 rounded-lg active:bg-blue-100 bg-blue-50 disabled:bg-slate-50 disabled:text-slate-300" disabled={activeIndex === 0}>
         &lt;
       </button>
-      <button onClick={() => swiper.slideNext()} className="r-nextButton">
+      <button onClick={() => swiper.slideNext()}
+       className="transition-all text-blue-600 py-2 px-4 rounded-lg active:bg-blue-100 bg-blue-50 disabled:bg-slate-50 disabled:text-slate-300" disabled={activeIndex === 2}>
         &gt;
       </button>
     </div>
